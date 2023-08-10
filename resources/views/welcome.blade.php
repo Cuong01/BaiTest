51394cb131199cef0cfa5c3e43d7f19d0b624ede
Cường <cuongds12000@gmail.com>
2023-08-10 08:54:30 +0700

    a

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Trang chủ</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+    <link rel="stylesheet" href="../css/style.css">
+
+</head>
+
+<body>
+    <div class="nav0">
+        <img src="../LOGO_MIRAWAY.svg" alt="">
+        <ul>
+            <li>Giới thiệu</li>
+            <li>Giải pháp</li>
+            <li>Tin tức & Sự kiện</li>
+            <li>Cơ hội nghề nghiệp</li>
+            <li>Đăng nhập</li>
+        </ul>
+    </div>
+    <div>
+        <div class="content">
+            <div class="content1">
+                <img src="../image/tichophethong.jpg" alt="" height="443px" width="789px">
+                <h3>DATA – TRÁI TIM CỦA CÔNG NGHỆ CHUYỂN ĐỔI SỐ</h3>
+                <p>Chuyển đổi công nghệ số đang trở thành xu hướng không thể tránh khỏi đối với các doanh nghiệp trong
+                    thời đại ngày nay. Với sự phát triển của 4.0 thì chuyển đổi số đã không còn là sự lựa chọn mà là con
+                    đường bắt buộc của những doanh nghiệp muốn thành công. Vậy phải làm thế nào? Dựa trên cái gì? Hãy
+                    cùng Miraway tìm hiểu</p>
+                <div class="frame1">
+                    <p>_ 07/07/2023_ Phạm Uyên_ 0 Comments</p>
+                    <a href="">Xem thêm</a>
+                </div>
+            </div>
+            <div>
+                <div class="content2">
+                    <img src="../image/tichophethong.jpg" alt="" height="140px" width="379px">
+                    <h4>Ứng dụng CDP phân tích nguyện vọng và hành vi của khách hàng lĩnh vực tài chính ngân hàng</h4>
+
+                    <div class="frame1">
+                        <p>_ 07/07/2023_</p>
+                        <a href="">Xem thêm</a>
+                    </div>
+                </div>
+                <div class="content2">
+                    <img src="../image/tichophethong.jpg" alt="" height="140px" width="379px">
+                    <h4>Miraway tài trợ hội thảo IDG – Chuyển đổi số trong lĩnh vực Tài chính – Ngân hàng và Fintech –
+                        Dữ liệu cá nhân</h4>
+                    <div class="frame1">
+                        <p>_ 07/07/2023_</p>
+                        <a href="">Xem thêm</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content3">
+            <h1>Miraway trên mạng xã hội</h1>
+            <div class="all_icon">
+                <div class="icon"><i class="fa-brands fa-youtube"> </i></div>
+                <div class="icon"><i class="fa-brands fa-facebook"> </i></div>
+                <div class="icon"><i class="fa-brands fa-linkedin-in"> </i></div>
+                <div class="icon">Zalo</div>
+            </div>
+        </div>
+        <div class="content4">
+            <div class="bag1">
+                <div class="bag_s">
+                    <div class="frame2">
+                        <h1>Bài viết tiêu biểu</h1>
+                        <div class="tow_icon">
+                            <div class="icon1"><i class="fa-solid fa-arrow-left"> </i></div>
+                            <div class="icon1"><i class="fa-solid fa-arrow-right"> </i></div>
+                        </div>
+                    </div>
+                    <div class="frame3">
+                        <img src="../image/tichophethong.jpg" alt="" height="217px" width="277px">
+                        <div>
+                            <h4>TƯNG BỪNG KHAI TRƯƠNG KIOSK TỰ PHỤC VỤ 24/7</h4>
+                            <p>Ngày 20/8 vừa qua, Công ty Cổ phần Miraway Giải pháp Công nghệ đã triển khai thành công
+                                Kiosk tự phục vụ 24/7 (Self – Service Kiosk) tại 30 cửa hàng lớn thuộc hệ thống
+                                Winmart+.</p>
+                            <div class="a1"><a href="">Xem thêm</a></div>
+                        </div>
+                    </div>
+                    <div class="frame3">
+                        <img src="../image/tichophethong.jpg" alt="" height="217px" width="277px">
+                        <div>
+                            <h4>TƯNG BỪNG KHAI TRƯƠNG KIOSK TỰ PHỤC VỤ 24/7</h4>
+                            <p>Ngày 20/8 vừa qua, Công ty Cổ phần Miraway Giải pháp Công nghệ đã triển khai thành công
+                                Kiosk tự phục vụ 24/7 (Self – Service Kiosk) tại 30 cửa hàng lớn thuộc hệ thống
+                                Winmart+.</p>
+                            <div class="a1"><a href="">Xem thêm</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bag_s">
+                    <div class="frame2">
+                        <h1>Bài viết Mới nhất</h1>
+                        <div class="tow_icon">
+                            <div class="icon1"><i class="fa-solid fa-arrow-left"> </i></div>
+                            <div class="icon1"><i class="fa-solid fa-arrow-right"> </i></div>
+                        </div>
+                    </div>
+                    <div class="frame3">
+                        <img src="../image/tichophethong.jpg" alt="" height="217px" width="277px">
+                        <div>
+                            <h4>TƯNG BỪNG KHAI TRƯƠNG KIOSK TỰ PHỤC VỤ 24/7</h4>
+                            <p>Ngày 20/8 vừa qua, Công ty Cổ phần Miraway Giải pháp Công nghệ đã triển khai thành công
+                                Kiosk tự phục vụ 24/7 (Self – Service Kiosk) tại 30 cửa hàng lớn thuộc hệ thống
+                                Winmart+.</p>
+                            <div class="a1"><a href="">Xem thêm</a></div>
+                        </div>
+                    </div>
+                    <div class="frame3">
+                        <img src="../image/tichophethong.jpg" alt="" height="217px" width="277px">
+                        <div>
+                            <h4>TƯNG BỪNG KHAI TRƯƠNG KIOSK TỰ PHỤC VỤ 24/7</h4>
+                            <p>Ngày 20/8 vừa qua, Công ty Cổ phần Miraway Giải pháp Công nghệ đã triển khai thành công
+                                Kiosk tự phục vụ 24/7 (Self – Service Kiosk) tại 30 cửa hàng lớn thuộc hệ thống
+                                Winmart+.</p>
+                            <div class="a1"><a href="">Xem thêm</a></div>
+                        </div>
+                    </div>
+                    <div class="frame3">
+                        <img src="../image/tichophethong.jpg" alt="" height="217px" width="277px">
+                        <div>
+                            <h4>TƯNG BỪNG KHAI TRƯƠNG KIOSK TỰ PHỤC VỤ 24/7</h4>
+                            <p>Ngày 20/8 vừa qua, Công ty Cổ phần Miraway Giải pháp Công nghệ đã triển khai thành công
+                                Kiosk tự phục vụ 24/7 (Self – Service Kiosk) tại 30 cửa hàng lớn thuộc hệ thống
+                                Winmart+.</p>
+                            <div class="a1"><a href="">Xem thêm</a></div>
+                        </div>
+                    </div>
+                    <div class="frame3">
+                        <img src="../image/tichophethong.jpg" alt="" height="217px" width="277px">
+                        <div>
+                            <h4>TƯNG BỪNG KHAI TRƯƠNG KIOSK TỰ PHỤC VỤ 24/7</h4>
+                            <p>Ngày 20/8 vừa qua, Công ty Cổ phần Miraway Giải pháp Công nghệ đã triển khai thành công
+                                Kiosk tự phục vụ 24/7 (Self – Service Kiosk) tại 30 cửa hàng lớn thuộc hệ thống
+                                Winmart+.</p>
+                            <div class="a1"><a href="">Xem thêm</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bag2">
+                <div class="register">
+                    <h2>Đăng ký tài khoản viết bài</h2>
+                    <div class="item1">
+                        <div class="label">Họ và tên</div>
+                        <input type="text">
+                    </div>
+                    <div class="item1">
+                        <div class="label">Email</div>
+                        <input type="text">
+                    </div>
+                    <div class="item1">
+                        <div class="label">Mật khẩu</div>
+                        <input type="password">
+                    </div>
+                    <div class="button_dk">Đăng ký</div>
+                </div>
+                <div class="content5">
+                    <div class="frame4">
+                        <i class="fa-solid fa-book-open"> </i>
+                        <h2>Xem thêm</h2>
+                    </div>
+                    <div>
+                        <div class="item2">
+                            <a href="">Thông báo Hợp tác chiến lược giữa Miraway – Bắc Hải –
+                                Viettelconstruction</a>
+                        </div>
+                        <div class="item2">
+                            <a href="">Thông báo Hợp tác chiến lược giữa Miraway – Bắc Hải –
+                                Viettelconstruction</a>
+                        </div>
+                        <div class="item2">
+                            <a href="">Thông báo Hợp tác chiến lược giữa Miraway – Bắc Hải –
+                                Viettelconstruction</a>
+                        </div>
+                        <div class="item2">
+                            <a href="">Thông báo Hợp tác chiến lược giữa Miraway – Bắc Hải –
+                                Viettelconstruction</a>
+                        </div>
+                        <div class="item2">
+                            <a href="">Thông báo Hợp tác chiến lược giữa Miraway – Bắc Hải –
+                                Viettelconstruction</a>
+                        </div>
+                        <div class="item2">
+                            <a href="">Thông báo Hợp tác chiến lược giữa Miraway – Bắc Hải –
+                                Viettelconstruction</a>
+                        </div>
+                        <div class="item2">
+                            <a href="">Thông báo Hợp tác chiến lược giữa Miraway – Bắc Hải –
+                                Viettelconstruction</a>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="content6">
+                    <div class="frame4">
+                        <i class="fa-solid fa-tags"> </i>
+                        <h2>TAG</h2>
+                    </div>
+                    <div class="tag">
+                        <div class="tag1">
+                            <h3>CDP</h3>
+                            <h3>CETM</h3>
+                            <h3>IDG</h3>
+                        </div>
+                        <div class="tag1">
+                            <h3>Miraway</h3>
+                            <h3>Tài chính</h3>
+                        </div>
+                        <div class="tag1">
+                            <h3>Trải nghiệm khách hàng</h3>
+                        </div>
+                        <div class="tag1">
+                            <h3>Hội thảo</h3>
+                            <h3>Dữ liệu khách hàng</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
